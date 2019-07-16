cdd450d0423e525a2ec106bd8603a4c3b32d989a
Shankar Anbalagan <bravoshankar@gmail.com>
2019-07-16 11:33:57 +0530

    It's working but not in the right way

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,33 +5,42 @@ class Content extends React.Component{
 
     url_a="https://www.food2fork.com/api/search?key=97753ac532afaee19e10e0bd53e215e4&sort=r";
     url_b="https://www.food2fork.com/api/search?key=b0411751579e25ce6de6f05b965dba21&sort=r";
+    elements=[];
+    json=null;
     constructor(props){
         super(props);
-        this.state='';
+        //this.state={json:null};
+        this.getRecipe=this.getRecipe.bind(this);
     }
+    /*
+    async componentDidMount(){
+        await this.getRecipe();
+    }
+    */
 
     async getRecipe(){
         try{
             var res=await fetch(this.url_b);
-            var json = await res.json();
-            return json;
+            this.setState({json:(await res.json())});
         }catch(err){
             alert("Oops something went wrong!");
         }
     }
 
     render(){
-        var elements=[];
+        //this.elements=[];
+        console.log("again");
         (async ()=>{
-            var d = await this.getRecipe();
+            await this.getRecipe();
             for(var i=0;i<10;i++){
-                //console.log(d["recipes"][i]["title"]);
-                elements.push(<Recipe title={d["recipes"][i]["title"]}/>);
-                console.log(elements[i]);
+                //this.elements.push(<Recipe title={this.state.json["recipes"][i]["title"]}/>);
+                //console.log(this.elements[i]);
+                this.elements.push(<Recipe title={"You're breathtaking!"}/>);
             }
-        })();
-        return(<div>
-            {elements}
+        }
+        )();
+        return (<div>
+            {this.elements}
         </div>);
     }
 }
